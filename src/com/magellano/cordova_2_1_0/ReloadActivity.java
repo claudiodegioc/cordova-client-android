9909e59351a6f9c4d4b7cc2caf173fd3fa6fe366
Calinos <claudiodegio@gmail.com>
2012-10-13 12:23:36 +0200

    Add new reload wating dialog

--- src/com/magellano/cordova_2_1_0/ReloadActivity.java
+++ src/com/magellano/cordova_2_1_0/ReloadActivity.java
@@ -1,24 +1,34 @@
 package com.magellano.cordova_2_1_0;
 
+import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.DroidGap;
+import org.apache.cordova.api.CordovaInterface;
+import org.apache.cordova.api.IPlugin;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.Toast;
-
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import com.magellano.cordova_2_1_0.ReloadHttpServer.OnReloadListener;
 
-public class ReloadActivity extends DroidGap implements OnReloadListener {
+public class ReloadActivity extends Activity implements CordovaInterface,
+		OnReloadListener {
+
+	final static private String TAG = "ReloadActivity";
 
 	private ReloadHttpServer reloadServer = new ReloadHttpServer();
 
@@ -26,17 +36,25 @@ public class ReloadActivity extends DroidGap implements OnReloadListener {
 
 	private String server = null;
 
+	private CordovaWebView cwv = null;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_reload);
+		cwv = (CordovaWebView) findViewById(R.id.wvCordova);
+		cwv.setWebViewClient(new ReloadWebViewClient());
+
+		// setContentView(R.layout.activity_reload);
 		server = getIntent().getStringExtra(Constant.EXTRA_RELOAD_SERVER);
 		// Start reload server
 		reloadServer.setListener(this);
 		reloadServer.start();
 
 		connect();
-		reload(false);
+		reload();
 	}
 
 	@Override
@@ -49,7 +67,7 @@ public class ReloadActivity extends DroidGap implements OnReloadListener {
 		runOnUiThread(new Runnable() {
 
 			public void run() {
-				reload(true);
+				reload();
 			}
 		});
 	}
@@ -61,7 +79,7 @@ public class ReloadActivity extends DroidGap implements OnReloadListener {
 		builder.setPositiveButton(android.R.string.ok, new OnClickListener() {
 
 			public void onClick(DialogInterface dialog, int which) {
-				ReloadActivity.this.endActivity();
+				ReloadActivity.this.finish();
 			}
 		});
 		// Create the AlertDialog object and return it
@@ -80,7 +98,7 @@ public class ReloadActivity extends DroidGap implements OnReloadListener {
 		// Handle item selection
 		switch (item.getItemId()) {
 		case R.id.menuReload:
-			reload(true);
+			reload();
 			return true;
 		case R.id.menuConnect:
 			connect();
@@ -90,13 +108,9 @@ public class ReloadActivity extends DroidGap implements OnReloadListener {
 		}
 	}
 
-	private void reload(boolean showToast) {
-		loadUrl("http://" + server + ":1337/index.html");
-		if (showToast) {
-			Toast.makeText(ReloadActivity.this, "Reload Done",
-					Toast.LENGTH_SHORT).show();
-		}
-
+	private void reload() {
+		cwv.loadUrl("http://" + server + ":1337/index.html");
+		findViewById(R.id.rlLoading).setVisibility(View.VISIBLE);
 	}
 
 	private void connect() {
@@ -108,6 +122,43 @@ public class ReloadActivity extends DroidGap implements OnReloadListener {
 		} catch (Exception e) {
 			Log.e(TAG, e.getMessage());
 			showDialog(ERROR_DIALOG);
+		}
+	}
+
+	@Deprecated
+	public void cancelLoadUrl() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Activity getActivity() {
+		// TODO Auto-generated method stub
+		return this;
+	}
+
+	@Deprecated
+	public Context getContext() {
+		return this;
+	}
+
+	public Object onMessage(String arg0, Object arg1) {
+		return null;
+	}
+
+	public void setActivityResultCallback(IPlugin arg0) {
+
+	}
+
+	public void startActivityForResult(IPlugin arg0, Intent arg1, int arg2) {
+
+	}
+
+	private class ReloadWebViewClient extends WebViewClient {
+
+		@Override
+		public void onPageFinished(WebView view, String url) {
+			findViewById(R.id.rlLoading).setVisibility(View.INVISIBLE);
+			super.onPageFinished(view, url);
 		}
 	}
 }
